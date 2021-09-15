d28df38bce4f128fc2b7659c4a069f85def61160
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-15 21:35:49 +0800

    Сделаны страницы emptylayout, mainlayout компоненты categories, login подключены иконки

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div id="app">    
-    <button class="btn">Test</button>
+  <div id="app">
+    <component :is="layout">
+      <router-view/>
+    </component>
+
     <router-view/>
   </div>
 </template>
+
+<script>
+import EmptyLayout from '@/layouts/EmptyLayout'
+import MainLayout from '@/layouts/MainLayout'
+export default {
+  computed: {
+    layout(){
+      console.log(this.$route.meta)
+      return (this.$route.meta.layout || 'empty') + '-layout'
+    }
+  },
+  components: {
+    EmptyLayout, MainLayout
+  }
+}
+</script>
 
 <style lang="scss">
 @import '~materialize-css/dist/css/materialize.min.css';
