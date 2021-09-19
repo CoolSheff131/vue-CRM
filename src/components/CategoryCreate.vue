25a1d516242c44585cfd34679b9708e0eb5cd62f
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-19 15:08:16 +0800

    Добавлена валидация формы categoryCreate

--- src/components/CategoryCreate.vue
+++ src/components/CategoryCreate.vue
@@ -5,23 +5,31 @@
             <h4>Создать</h4>
           </div>
 
-          <form>
+          <form @submit.prevent="submitHandler">
             <div class="input-field">
               <input
                   id="name"
                   type="text"
+                  v-model="title"
+                  :class="{invalid: $v.title.$dirty && !$v.title.required}"
               >
               <label for="name">Название</label>
-              <span class="helper-text invalid">Введите название</span>
+              <span
+                v-if="$v.title.$dirty && !$v.title.required"              
+               class="helper-text invalid">Введите название категории</span>
             </div>
 
             <div class="input-field">
               <input
                   id="limit"
                   type="number"
+                  v-model="limit"
+                  :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
               >
               <label for="limit">Лимит</label>
-              <span class="helper-text invalid">Минимальная величина</span>
+              <span 
+              v-if="$v.limit.$dirty && !$v.limit.required"
+              class="helper-text invalid">Минимальная значение {{$v.limit.$params.minValue.min}}</span>
             </div>
 
             <button class="btn waves-effect waves-light" type="submit">
@@ -32,3 +40,28 @@
         </div>
       </div>
 </template>
+
+<script>
+import {required, minValue} from 'vuelidate/lib/validators'
+export default {
+  data: ()=> ({
+    title:'',
+    limit: 100
+  }),
+  validations:{
+    title: {required},
+    limit: {minValue: minValue(100)}
+  },
+  mounted(){
+    M.updateTextFields()
+  },
+  methods:{
+    submitHandler(){
+      if(this.$v.$invalid){
+        this.$v.$touch()
+        return
+      }
+    }
+  }
+}
+</script>
