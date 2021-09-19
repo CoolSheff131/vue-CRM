f94a27d35e45583d4345f92691bbb2a1265854ac
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-19 13:35:57 +0800

    Добавлена обработка ошибок

--- src/store/auth.js
+++ src/store/auth.js
@@ -8,11 +8,12 @@ export default {
       try{
         await firebase.auth().signInWithEmailAndPassword(email,password)
       }catch(e){
+        commit('setError', e)
         throw e
       }
     }
   },
-  async register({dispatch},{email,password,name}){
+  async register({dispatch, commit},{email,password,name}){
     try{
       await firebase.auth().createUserWithEmailAndPassword(email,password)
       const uid = await dispatch('getUid')
@@ -21,6 +22,7 @@ export default {
         name
       })
     }catch(e){
+      commit('setError', e)
       throw e
     }
   },
