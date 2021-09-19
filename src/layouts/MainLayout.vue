b4ab5975526be599e7ac63cc657a2d3d8ce30b52
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-19 14:44:53 +0800

    Реализовано обновление страницы home

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="app-main-layout">
+<div> 
+  <Loader v-if="loading"/>
+  <div class="app-main-layout" v-else>
 
     <Navbar @click="isOpen = !isOpen"/>
 
@@ -17,6 +19,7 @@
       </router-link>
     </div>
   </div>
+  </div>
 </template>
 
 
@@ -26,12 +29,15 @@ import SideBar from '../components/app/Sidebar.vue'
 export default {
   name: 'main-layout',
   data: ()=> ({
-    isOpen:true
+    isOpen:true,
+    loading: true
   }),
   async mounted(){
     if(!Object.keys(this.$store.getters.info).length){
       await this.$store.dispatch('fetchInfo')
     }
+
+    this.loading = false
   },
   components: {
     Navbar, SideBar
