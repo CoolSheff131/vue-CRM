9b24bc80542bf3a55c996c14bb4ba88bfe32c909
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-20 20:53:35 +0800

    Добавлена валидация CategoryEdit запрос категории с сервера

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -7,25 +7,40 @@
 
           <form>
             <div class="input-field" >
-              <select>
-                <option>Category</option>
+              <select ref="select">
+                <option
+                  v-for="c of categories"
+                  :key="c.id"
+                  :value="c.id"
+                >{{c.title}}</option>
               </select>
               <label>Выберите категорию</label>
             </div>
 
             <div class="input-field">
-              <input type="text" id="name">
+              <input
+                  id="name"
+                  type="text"
+                  v-model="title"
+                  :class="{invalid: $v.title.$dirty && !$v.title.required}"
+              >
               <label for="name">Название</label>
-              <span class="helper-text invalid">TITLE</span>
+              <span
+                v-if="$v.title.$dirty && !$v.title.required"              
+               class="helper-text invalid">Введите название категории</span>
             </div>
 
             <div class="input-field">
               <input
                   id="limit"
                   type="number"
+                  v-model.number="limit"
+                  :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
               >
               <label for="limit">Лимит</label>
-              <span class="helper-text invalid">LIMIT</span>
+              <span 
+              v-if="$v.limit.$dirty && !$v.limit.required"
+              class="helper-text invalid">Минимальная значение {{$v.limit.$params.minValue.min}}</span>
             </div>
 
             <button class="btn waves-effect waves-light" type="submit">
@@ -36,3 +51,33 @@
         </div>
       </div>
 </template>
+
+<script>
+import {required, minValue} from 'vuelidate/lib/validators'
+export default {
+  props:{
+    categories:{
+      type: Array,
+      required: true
+    }
+  },
+  data: ()=>({
+    select: null,
+    title:'',
+    limit: 100
+  }),
+  validations:{
+    title: {required},
+    limit: {minValue: minValue(100)}
+  },
+  mounted(){
+    M.updateTextFields()
+    this.select = M.FormSelect.init(this.$refs.select)
+  },
+  destroyed(){
+    if(this.select && this.select.destroy){
+      this.select.destroy()
+    }
+  }
+}
+</script>
--- src/store/category.js
+++ src/store/category.js
@@ -1,6 +1,25 @@
 import firebase from 'firebase/app'
 export default {
   actions:{
+    async fetchCategories({commit, dispatch}){
+      try {
+        const uid = await dispatch('getUid')
+        const categories = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {}
+        // const cats = []
+        // Object.keys(categories).forEach(key => {
+        //   cats.push({
+        //     title: categories[key].title,
+        //     limit: categories[key].limit,
+        //     id: key
+        //   })
+        // })
+        // return cats
+        return Object.keys(categories).map(key =>({...categories[key], id: key}))
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
+    },
     async createCategory({commit, dispatch},{title, limit}){
       try {
         const uid = await dispatch('getUid')
