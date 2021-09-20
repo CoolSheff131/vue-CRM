6d8d8aebcf4fd825bc318515f54ccae0e0c89d68
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-20 21:29:44 +0800

    Сделана модел record

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -4,37 +4,45 @@
       <div class="page-title">
         <h3>Новая запись</h3>
       </div>
+      
+      <Loader v-if="loading"/>
 
-      <form class="form">
+      <p class="center" v-else-if="!categories.length">Категорий пока нет. <router-link to="/categories"> Добавить новую категорию </router-link> </p>
+
+      <form class="form" v-else>
         <div class="input-field">
-          <select>
-            <option>name cat</option>
+          <select ref="select" v-model="category">
+            <option
+              v-for="c in categories"
+              :key="c.id"
+              :value="c.id"
+            >{{c.title}}}</option>
           </select>
           <label>Выберите категорию</label>
         </div>
 
         <p>
           <label>
-            <input class="with-gap" name="type" type="radio" value="income" />
+            <input class="with-gap" name="type" type="radio" value="income" v-model="type" />
             <span>Доход</span>
           </label>
         </p>
 
         <p>
           <label>
-            <input class="with-gap" name="type" type="radio" value="outcome" />
+            <input class="with-gap" name="type" type="radio" value="outcome" v-model="type"/>
             <span>Расход</span>
           </label>
         </p>
 
         <div class="input-field">
-          <input id="amount" type="number" />
+          <input id="amount" type="number" v-model.number="amount" />
           <label for="amount">Сумма</label>
           <span class="helper-text invalid">amount пароль</span>
         </div>
 
         <div class="input-field">
-          <input id="description" type="text" />
+          <input id="description" type="text" v-model="description"/>
           <label for="description">Описание</label>
           <span class="helper-text invalid">description пароль</span>
         </div>
@@ -47,3 +55,34 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'record',
+  data: ()=> ({
+    loading: true,
+    select: null,
+    categories: [],
+    category: null,
+    type:'outcome',
+    amount: 1,
+    description: ''
+  }),
+  async mounted(){
+    this.categories = await this.$store.dispatch('fetchCategories')
+    this.loading = false
+
+    if(this.categories.length){
+      this.category = this.categories[0]
+    }
+    setTimeout(()=>{
+      this.select = M.FormSelect.init(this.$refs.select)
+    },0)
+  },
+  destroyed(){
+    if(this.select && this.select.destroy){
+      this.select.destroy()
+    }
+  }
+}
+</script>
