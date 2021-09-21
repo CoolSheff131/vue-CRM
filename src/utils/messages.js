ce1863e143809ade503af485fe0e15d398f5d90e
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-21 16:57:14 +0800

    Добавлена защита роутов

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,5 +2,6 @@ export default {
   'logout': 'Вы вышли из системы',
   'auth/user-not-found': 'Пользователя с таким email не существует',
   'auth/wrong-password': 'Неверный пароль',
+  'login': 'Для начала войдите в систему',
   'auth/email-already-in-use': 'Email уже занят'
 }
