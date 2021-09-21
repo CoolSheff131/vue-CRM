f43ad50e42f169cf9f083929f464a56df2b35482
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-21 16:49:46 +0800

    Создан tooltip

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -16,7 +16,7 @@
             <strong>{{cat.title}}:</strong>
             {{cat.spend | currency}} из {{cat.limit | currency}}
           </p>
-          <div class="progress">
+          <div class="progress" v-tooltip="cat.tooltip">
             <div class="determinate" :class="[cat.progressColor]" :style="{width: cat.progressPercent + '%'}"></div>
           </div>
         </div>
@@ -27,6 +27,7 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import currencyFilter from '@/filters/currency.filter'
 export default {
   name: 'planning',
   data: ()=>({
@@ -48,11 +49,15 @@ export default {
         const percent = 100 * spend / cat.limit
         const progressPercent = percent > 100? 100 : percent
         const progressColor = percent < 60 ? 'green': percent < 100 ?'yellow': 'red'
+
+        const tooltipValue = cat.limit - spend
+        const tooltip = `${tooltipValue < 0 ? 'Превышение на: ': 'Осталось: '} ${currencyFilter(Math.abs(tooltipValue))}`
         return{
           ...cat,
           progressPercent,
           progressColor,
-          spend
+          spend,
+          tooltip
         }
     })
     this.loading = false
